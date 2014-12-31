e7708bfbd103842d0235aba9f81cc506835c22f6
Vince Snow <mrussel@163.com>
2014-12-31 11:45:06 +0800

    Revert "update"
    This reverts commit 7611a9b7fc04bd0ca2638430d00996e80f7eac84.

--- script/jquery.dataview.js
+++ script/jquery.dataview.js
@@ -250,13 +250,8 @@
                 delete selfList.items[itemKey];
             };
 
-            //destroy all items
-            
-            
             //remove all items
             selfList.tBody.innerHTML = '';
-            
-            selfList.items = {};
 
             //create each items
             for (i in dataItems) {
@@ -336,7 +331,6 @@
                     dataType: 'json',
                     success: function (cb) {
                         if (cb.status === 'success') {
-                            
                             opts.result.create(cb.items);
                         }
                     }
